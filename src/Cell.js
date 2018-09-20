1d0edf539152eaf411eaaac0a42a198b2047719f
Haytham KATBY <mohakapt@gmail.com>
2018-09-20 14:30:54 +0300

    - Add custom action feature to the cell (To automatically handle and open links and urls)

--- src/Cell.js
+++ src/Cell.js
@@ -5,19 +5,45 @@ import {
 	TouchableHighlight,
 	TouchableNativeFeedback,
 	ViewPropTypes,
+	Linking,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
-const Cell = ({ children, style, isEnabled, onPress, onLongPress, underlayColor }) => {
+const Cell = ({ children, style, isEnabled, onPress, onLongPress, underlayColor, customAction, customActionType, customActionTrigger }) => {
 	const renderCell = () => (
 		<View style={style}>
 			{children}
 		</View>
 	);
 
-	if (!onPress) {
+	if (!onPress && !customAction) {
 		return renderCell();
 	}
+
+	const onCustomAction = () => {
+		let url;
+		switch (customActionType) {
+			case 'call':
+				url = `tel: ${customAction}`;
+				break;
+			case 'compose':
+				url = `mailto: ${customAction}`;
+				break;
+			case 'openUrl':
+				url = customAction;
+				break;
+		}
+
+		if (url) {
+			Linking.canOpenURL(url).then(canOpen => canOpen && Linking.openURL(url));
+		}
+
+		if (customActionTrigger === 'onPress') {
+			onPress && onPress();
+		} else if (customActionTrigger === 'onLongPress') {
+			onLongPress && onLongPress();
+		}
+	};
 
 	const Touchable = Platform.OS === 'ios' ? TouchableHighlight : TouchableNativeFeedback;
 	const touchableProps = Platform.select({
@@ -26,9 +52,16 @@ const Cell = ({ children, style, isEnabled, onPress, onLongPress, underlayColor 
 		},
 		android: {},
 	});
+	const onPressAction = (customAction && customActionTrigger === 'onPress') ? onCustomAction : onPress;
+	const onLongPressAction = (customAction && customActionTrigger === 'onLongPress') ? onCustomAction : onLongPress;
 
 	return (
-		<Touchable {...touchableProps} disabled={!isEnabled} onPress={onPress} onLongPress={onLongPress}>
+		<Touchable
+			{...touchableProps}
+			disabled={!isEnabled}
+			onPress={onPressAction}
+			onLongPress={onLongPressAction}>
+
 			{renderCell()}
 		</Touchable>
 	);
@@ -44,10 +77,17 @@ Cell.propTypes = {
 	isEnabled: PropTypes.bool,
 	onPress: PropTypes.func,
 	onLongPress: PropTypes.func,
+
+	customAction: PropTypes.string,
+	customActionType: PropTypes.oneOf(['call', 'compose', 'openUrl']),
+	customActionTrigger: PropTypes.oneOf(['onPress', 'onLongPress']),
 };
 
 Cell.defaultProps = {
 	isEnabled: true,
+
+	customActionType: 'openUrl',
+	customActionTrigger: 'onPress',
 };
 
 export default Cell;
