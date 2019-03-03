69f90a54b7f2dc84b6bc01a63d598e3a1f505d70
Haytham KATBY <mohakapt@gmail.com>
2019-03-03 19:03:47 +0300

    Remove node_modules and package.json from example

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@
 
 import { AppRegistry } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-import App from './App';
+import App from './src/App';
 import { name as appName } from './app.json';
 
 const AppNavigator = createStackNavigator({ App: { screen: App } });
