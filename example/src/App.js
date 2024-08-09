1d54b9c61bade7020eb24f1023b5c51010126028
Haytham KATBY <mohakapt@gmail.com>
2024-08-09 17:43:04 +0300

    feat: support dark mode in the example

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,47 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 
+import { getColorPalette } from './assets/colors';
 import ThemeContext from './tools/ThemeContext';
 import Stack from './tools/NavigationStack';
 import MainScreen from './MainScreen';
 
+const getTheme = (theme) => {
+	const palette = getColorPalette(theme);
+	const parent = theme === 'light' ? DefaultTheme : DarkTheme;
+
+	return {
+		...parent,
+		colors: {
+			...parent.colors,
+			primary: palette.accent,
+			text: palette.headerText,
+			border: palette.headerSeparator,
+		},
+	};
+};
+
+const lightTheme = getTheme('light');
+const darkTheme = getTheme('dark');
+const midnightTheme = getTheme('midnight');
+
 function App() {
 	// possible values; 'light', 'dark', 'midnight'
 	const [theme, setTheme] = useState('light');
+	const navigationTheme = useMemo(() => {
+		switch (theme) {
+			case 'dark':
+				return darkTheme;
+			case 'midnight':
+				return midnightTheme;
+			default:
+				return lightTheme;
+		}
+	}, [theme]);
 
 	return (
 		<ThemeContext.Provider value={[theme, setTheme]}>
-			<NavigationContainer theme={theme === 'light' ? DefaultTheme : DarkTheme}>
+			<NavigationContainer theme={navigationTheme}>
 				<Stack.Navigator>
 					<Stack.Screen name='Main' component={MainScreen} />
 				</Stack.Navigator>
